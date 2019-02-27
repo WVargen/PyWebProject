7cce2801710da0cb64f7c7ad9a5485d6975a84ca
WVargen <sacred_y@163.com>
2019-02-27 17:53:42 +0800

    update

--- server/DispatchServer.py
+++ server/DispatchServer.py
@@ -8,24 +8,38 @@ from os import path
 from urllib.parse import urlparse
 import logging
 
-paths = {
-            '/home'          : {'status': 200, 'mimetype' :'text/html', 'html_path': '../web_root/static/index.html'},
-            '/css/style.css' : {'status': 200, 'mimetype' :'text/css', 'html_path': '../web_root/css/style.css'},
+MIMETYPE = {
+               'html' : 'text/html',
+               'css'  : 'text/css',
+               'txt'  : 'text/plain',
+               'mpeg' : 'video/mpeg',
+               'mpg'  : 'video/mpeg',
+               'mov'  : 'video/quicktime'
+            }
+  
+pathsMap = {
+            '/favicon.ico'   : {'status': 200, 'html_path': '../web_root/images/wb_favicon.ico'},
+            '/home'          : {'status': 200, 'mimetype' : MIMETYPE['html'], 'html_path': '../web_root/static/index.html'},
+            '/css/style.css' : {'status': 200, 'mimetype' : MIMETYPE['css'], 'html_path': '../web_root/css/style.css'},
+            
+            '/login'         : {'status': 200},
+            
             '/foo'           : {'status': 200},
             '/bar'           : {'status': 302},
             '/baz'           : {'status': 404},
             '/qux'           : {'status': 500}
         }
 
+
 class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', MIMETYPE['html'])
         self.end_headers()
 
     def do_GET(self):
-        if self.path in paths:
-            self.respond(paths[self.path])
+        if self.path in pathsMap:
+            self.respond(pathsMap[self.path])
         else:
             self.respond({'status': 500})
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
@@ -34,7 +48,11 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) #  Gets the size of data
         post_data = self.rfile.read(content_length) #  Gets the data itself
-
+        
+        if self.path in pathsMap:
+            if self.path == '/login':
+                
+                print("11111")
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
 
@@ -47,17 +65,24 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
     def handle_http(self, status_code, path):
         self.send_response(status_code)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        
         htmlContent = ''
         htmlPath = ''
-        if paths[self.path]['mimetype']:
-            self.send_header('Content-type', paths[self.path]['mimetype'])
-        if paths[self.path]['html_path']:
-            htmlPath = paths[self.path]['html_path']
-            with open(htmlPath, 'rb') as f:
-                htmlContent = f.read()
+        if self.path in pathsMap:
+            p = pathsMap[self.path]
+            if 'mimetype' in p:
+                mimetype = p['mimetype']
+                self.send_header('Content-type', mimetype)
+            else:
+                self.send_header('Content-type', MIMETYPE['html'])
+            if 'html_path' in p:
+                htmlPath = p['html_path']
+                with open(htmlPath, 'rb') as f:
+                    htmlContent = f.read()
+            self.end_headers()
         else:
+            self.send_header('Content-type', MIMETYPE['html'])
+            self.end_headers()
             htmlContent = '''
            <html><head><title>Title goes here.</title></head>
            <body><p>This is a test.</p>
